557576aa38c8366149fcb9dffc2c4f600e9b01c9
Tabs Macedo <tabata.macedo1897@gmail.com>
2025-04-03 16:09:23 -0300

    adições do banco de dados

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,24 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./Menu.module.css";
 
 const Menu = ({ items }) => {
+  
   return (
     <div className={styles.menu}>
       <h2>Nosso Cardápio</h2>
       <div className={styles.cardContainer}>
         {items.map((item) => (
-          <div key={item.id} className={styles.card}>
-            <Image
-              src={item.img}
-              alt={item.name}
-              width={150}
-              height={item.imgHeight}
-            />
+          <div key={item.id} className={styles.card} onClick={() => addToCart(item)}>
+            <Image src={item.img} alt={item.name} width={150} height={150} />
             <h3>{item.name}</h3>
             <p>{item.price}</p>
           </div>
         ))}
       </div>
+
     </div>
   );
 };
